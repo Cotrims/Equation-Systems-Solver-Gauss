f269bd66697cde3eb8a1ffe6cc16f466a5224f86
Cotrims <49480518+Cotrims@users.noreply.github.com>
2019-11-03 12:24:25 -0300

    ....

--- matriz/Matriz.java
+++ matriz/Matriz.java
@@ -55,7 +55,7 @@ public class Matriz
         return this.col;
     }
 
-    /*public double[] Escalona() 
+    public double[] Escalona() 
     { 
         String arq; 
         
@@ -73,7 +73,7 @@ public class Matriz
                 }
             }
         }
-    }*/
+    }
 
     public boolean isColunaComZero(double[] col, int lin) throws Exception 
     {
